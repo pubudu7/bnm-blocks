f58780c7244d4f3bed20dde6e05c273d2d0448e8
Pubudu Malalasekara <themezhut@gmail.com>
2023-08-10 21:00:15 +0530

    Removed posts-grid remainders

--- build/blocks/posts/posts-grid/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6fad6a5c122783b86c96');
--- build/blocks/posts/posts-grid/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '6bd456bbca46e3d48547');
